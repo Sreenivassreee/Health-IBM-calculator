470dbee3f72d23c2a4f43fe184119c4d68016824
Sreenivassreee <ksreenivascse@gmail.com>
2021-03-27 20:53:52 +0530

    BMI calculation, nav to next screen completed

--- Health IBM Calculator/Controller/ViewController.swift
+++ Health IBM Calculator/Controller/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var WeightSlider: UISlider!
+    @IBOutlet weak var HeightSlider: UISlider!
+    
+    @IBOutlet weak var HeightLable: UILabel!
+    
+    
+    @IBOutlet weak var WeightLable: UILabel!
+    
+    var height=0.0
+    var weight=0.0
+    var bmi:Double=0.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        WeightLable.text="0 KG"
+        HeightLable.text="0 M"
+        
     }
-
-
+    
+    @IBAction func Height(_ sender: UISlider) {
+        height=Double(sender.value)*10
+        
+        HeightLable.text = String(format: "%.1f" , height) + " M"
+    }
+    
+    @IBAction func Weight(_ sender: UISlider) {
+        weight=Double(sender.value)*100
+        WeightLable.text = String(format: "%.1f" , weight) + " KG"
+        
+        
+    }
+    @IBAction func Calculate(_ sender: UIButton) {
+        print(height)
+        print(weight)
+        bmi = weight/(height*height)
+        print(bmi)
+        
+        self.performSegue(withIdentifier: "secondView", sender: self)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "secondView" {
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.bmi=String(format: "%.2f", bmi)
+        }
+    }
+    
 }
 
